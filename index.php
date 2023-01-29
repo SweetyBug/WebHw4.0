c217c4789c0350d52cc8f1cd4823bcce88a0212f
SweetyBug <maxim_terekhin@bk.ru>
2023-01-30 01:19:41 +0300

    Finish code

---
+++ index.php
@@ -0,0 +1,161 @@
+<?php
+    $work_expiriens = [
+        [
+            'post' => "Front End Developer",
+            'date_start' => "Jan 2015 - ",
+            'date_end' => "Current",
+            'discription' => "Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.",
+        ],
+        [
+            'post' => "Web Developer / something.com",
+            'date_start' => "Mar 2012 - ",
+            'date_end' => "Dec 2014",
+            'discription' => "Consectetur adipisicing elit. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.",
+        ],
+        [
+            'post' => "Graphic Designer / designsomething.com",
+            'date_start' => "Jun 2010 - ",
+            'date_end' => "Mar 2012",
+            'discription' => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. ",
+        ],
+    ];
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Мое резюме</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Jost:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script type="text/javascript" src="/FD126C42-EBFA-4E12-B309-BB3FDD723AC1/main.js?attr=TOHGg43aQuQlpqSBKLJ-wqHRTeAqaWJSQCDxO4wumkRRKPnV9vitgo65BlsMN3kPoC0cerpo0pyt0qqH45Lxx21eqDvS_UAZsE0zB97kv28HJhwOVZIDNhMXf7hu-vxmj-CFupR76BMlsfjFDkclpOSj5ViAWxLG6BT8JqFB1bS0wH7pRFN_E_VoJF36AFxZVre69yKCZgyB0xMeqvfz03L7m8OT7GJKnETDXGkX3HSl95gN_MKkfK8wVyjavQUTYJsd4ruj_L0x9_-BidQhRfr6TwUJWWrTc402vojeiwax9KXK_D9qTBJl1YShPliBEzaJ9sU2tTR4Fm_QK02_2rpSWMrDTSbtR8JWX1lpfgD0Z-PY5HH9VJqAjAD2ahSPsInjLxYLIKT0cjgc7YVKFdLSkBTTk_jfzNh3TuUzcJk2Z9PiEvvX4wDfZYbSrFfPvF1p2SwuJ6WEU7kqXGyVhOcxELDOH4Bn2N--ZSXDjs2qPLjEJcHzCNPaFbxZfdHChQnv11Rtr-EICw1YXW9Mrj0Pn6dWnqR_a5aYwCeG7KIsDbguq1M-I0S6FK8uc_Aj-r-1ig9Tli0hM2FT2c41liSR_9CUB4wWgEo-L1ZWXDUnBn7iDIbqTf1WhOa-9GT-CmzJ9Hp9lx-xnclJLoD0N1lBQJTsoTbV7-xo3idzODQcRRNLeFpnVfZbme_Ab_2a7gcnr9RUXuhG5zhSdJbwG-VJFl0SCNb0HeLR2IApiOGSU7UbNalOZqifaBvCHzL4E0pdXR7kXKpRLayhKJ7S4c0BG1rvehtRwpxTKK0ilsnl4j_g5EG8DplRSEFHsUcwLui59pxVDBoErkuYqUbzMn2d0JL8fpoj0_Fms4vmKWXyGS6YUgJ_ugC5ccbZr08oPHzGQo91VFuE_KUrCbQrgFgfl98aJwCMP-TxB3jS4mO9SUR4p0oL7ViTMBd-j_ySIqy966E5JPHIsg3wkSwM44i_D78_HiWMYVI40nRWokI-jL4O4UQR1A-nMDo9HcnhHzjqbCW9DzxqpCQyqCb-msDOxdbuTCV_r-3mP6SNifs" charset="UTF-8"></script><style>
+    html,body,h1,h2,h3,h4,h5,h6 {font-family: 'Jost', sans-serif;}
+    </style>
+  </head>
+<body class="w3-light-grey">
+
+<!-- Page Container -->
+<div class="w3-content w3-margin-top" style="max-width:1400px;">
+
+  <!-- The Grid -->
+  <div class="w3-row-padding">
+  
+    <!-- Left Column -->
+    <div class="w3-third">
+    
+      <div class="w3-white w3-text-grey w3-card-4">
+        <div class="w3-display-container">
+          <img src="https://imgfon.ru/Images/Details_img_880px/Animals/glaza-belyy_fon-kotik-morda-polosatyy.webp" style="width:100%" alt="Avatar">
+          <div class="w3-display-bottomleft w3-container w3-text-black">
+            <h2>Имя</h2>
+          </div>
+        </div>
+        <div class="w3-container">
+          <p><i class="fa fa-briefcase fa-fw w3-margin-right w3-large w3-text-teal"></i>Профессия</p>
+          <p><i class="fa fa-home fa-fw w3-margin-right w3-large w3-text-teal"></i>Город, страна</p>
+          <p><i class="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-teal"></i>E-mail</p>
+          <p><i class="fa fa-phone fa-fw w3-margin-right w3-large w3-text-teal"></i>Телефон</p>
+          <hr>
+
+          <p class="w3-large"><b><i class="fa fa-asterisk fa-fw w3-margin-right w3-text-teal"></i>Навыки</b></p>
+          <p>Adobe Photoshop</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:90%">90%</div>
+          </div>
+          <p>Photography</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:80%">
+              <div class="w3-center w3-text-white">80%</div>
+            </div>
+          </div>
+          <p>Illustrator</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:75%">75%</div>
+          </div>
+          <p>Media</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:50%">50%</div>
+          </div>
+          <br>
+
+          <p class="w3-large w3-text-theme"><b><i class="fa fa-globe fa-fw w3-margin-right w3-text-teal"></i>Языки</b></p>
+          <p>Английский</p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-round-xlarge w3-teal" style="height:24px;width:100%"></div>
+          </div>
+          <p>Испанский</p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-round-xlarge w3-teal" style="height:24px;width:55%"></div>
+          </div>
+          <p>Немецкий</p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-round-xlarge w3-teal" style="height:24px;width:25%"></div>
+          </div>
+          <br>
+        </div>
+      </div><br>
+
+    <!-- End Left Column -->
+    </div>
+
+    <!-- Right Column -->
+    <div class="w3-twothird">
+    
+      <div class="w3-container w3-card w3-white w3-margin-bottom">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Опыт работы</h2>
+        <?php for($i=0;$i<count($work_expiriens);$i++) :?>
+
+        <div class="w3-container">
+            <h5 class="w3-opacity"><b><?php echo $work_expiriens[$i]['post'] ?></b></h5>
+            <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i><?php echo $work_expiriens[$i]['date_start'] ?><?php if(strcasecmp($work_expiriens[$i]['date_end'], 'current') == 0) {?><span class="w3-tag w3-teal w3-round"><?php echo $work_expiriens[$i]['date_end'] ?></span></h6><?php } else {?><?php echo $work_expiriens[$i]['date_end'] ?></h6><?php }?>
+            <p><?php echo $work_expiriens[$i]['discription'] ?></p>
+            <hr>
+        </div>
+        <?php endfor;?>
+    
+
+      <div class="w3-container w3-card w3-white">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Образование</h2>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>gb.ru</b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Forever</h6>
+          <p>Web Development! All I need to know in one place</p>
+          <hr>
+        </div>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>London Business School</b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2013 - 2015</h6>
+          <p>Master Degree</p>
+          <hr>
+        </div>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>School of Coding</b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2010 - 2013</h6>
+          <p>Bachelor Degree</p><br>
+        </div>
+      </div>
+
+    <!-- End Right Column -->
+    </div>
+    
+  <!-- End Grid -->
+  </div>
+  
+  <!-- End Page Container -->
+</div>
+
+<!-- Footer -->
+<footer class="w3-container w3-teal w3-center w3-margin-top">
+  <p>Find me on social media.</p>
+  <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+  <i class="fa fa-twitter w3-hover-opacity"></i>
+  <i class="fa fa-linkedin w3-hover-opacity"></i>
+ <!-- End footer -->
+ </footer>
+
+</body>
+</html>
